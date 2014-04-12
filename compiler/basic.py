d81f284a19efc3670f3b7c3a6451148c44aa2117
rafica <rafica.ar@gmail.com>
2014-04-11 20:32:22 -0400

    Error handled started and worked for the basic program .. Still have to handle the ones taken care by JAVA

--- compiler/basic.py
+++ compiler/basic.py
@@ -160,6 +160,7 @@ lexer = lex.lex(optimize=1)
 #######################################################################
 
 driverNumber = 0
+errorFlag = 0
 
 # translation_unit:
 
@@ -288,6 +289,13 @@ def p_declaration_statement_1(p):
 
 def p_declaration_statement_2(p):
     'declaration_statement : type ID EQUALS assignment_expression SEMI'
+##    if p[1].lower()=='string':
+##        if not (p[4][0]=='"' and p[4][-1]=='"'):
+##            print 'String was not initialized by string in line number', p.lineno(2)
+            
+##    elif p[1].lower()=='int':
+    ## We will let java handle these kind of errors
+    
     p[0] = p[1] + ' ' + p[2] + ' = ' + p[4] + ' ;'
 
 # compound_statement:
@@ -324,26 +332,24 @@ def p_expression_3(p):
 # function_expression:
 def p_function_expression_1(p):
     'function_expression : ID LPAREN parameter_list RPAREN'
+    global errorFlag
+    if not p[3]=='' and (p[1]=='start' or p[1]=='close'):
+        print 'In line number',p.lineno(2),'...',p[1],'does not take any arguments'
+        errorFlag = 1
+    
     if p[1]=='start':
-        if not p[3]=='':
-            raise Exception('Start doesnot take arguments ...')
-        else:
-            global driverNumber
-            driverNumber+=1
-            p[0] = 'WebDriver driver'+ str(driverNumber) +' = new ChromeDriver()'
+        global driverNumber
+        driverNumber+=1
+        p[0] = 'WebDriver driver'+ str(driverNumber) +' = new ChromeDriver()'
             
-    elif p[1]=='open':
-        try:
-            global driverNumber
-            url = str(p[3])
-            p[0] = 'driver'+ str(driverNumber) +'.get('+p[3]+')'
-        except:
-            raise Exception('The URL parameter passed should be a string ...')
+    elif p[1]=='open':                      ###################  JAVA will handle the error for this ; errors can be : 1) URL might not be a string, 2) Mutiple parameters might be passed.
+        global driverNumber
+        p[0] = 'driver'+ str(driverNumber) +'.get('+p[3]+')'
 
     elif p[1]=='close':
-            global driverNumber
-            p[0] = 'driver'+ str(driverNumber) +'.close()'
-            driverNumber = driverNumber - 1
+        global driverNumber
+        p[0] = 'driver'+ str(driverNumber) +'.close()'
+        driverNumber = driverNumber - 1
     else:
         p[0] = p[1] + ' ( ' + p[3] + ' ) '
 
