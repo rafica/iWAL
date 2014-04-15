63aa683768bac5ddddd72a341a92e16e68a9e051
nr2476 <nr2476@columbia.edu>
2014-04-14 23:12:24 -0400

    commit take 2

--- compiler/basic.py
+++ compiler/basic.py
@@ -251,7 +251,7 @@ def p_type_3(p):
 
 def p_type_4(p):
     'type : STRING'
-    p[0] = p[1]
+    p[0] = 'String'
 
 def p_type_5(p):
     'type : KEY'
@@ -360,22 +360,20 @@ def p_function_expression_1(p):
     global driverNumber
     
     param = getParameterList(p[3])
-    if len(param)==1:
-        param = param[0]
     if (p[1]=='start' or p[1]=='close' or p[1]=='click') and not p[3]=='':
         print 'In line number',p.lineno(2),'...',p[1],'does not take any arguments'
         errorFlag = 1
 
     elif (p[1]=='input' or p[1]=='clickE' or p[1]=='tab') and (not len(param)==1 or param[0]==''):
         print 'In line number',p.lineno(2),'...',p[1],'takes 1 argument'
-        param = '"error"'
+        param = ['"error"']
         errorFlag = 1
 
     elif (not len(param)==2 or param[0]=='') and (p[1]=='inputE'):                                                  ## If your function needs exactly 2 parameters append it here
         print 'In line number',p.lineno(2),'...',p[1],'takes 2 arguments'
         param = ['"error1"', '"error2"']
         errorFlag = 1
-    
+
     if p[1]=='start':
         driverNumber+=1
         p[0] = 'WebDriver driver'+ str(driverNumber) +' = new ChromeDriver()'
@@ -388,10 +386,10 @@ def p_function_expression_1(p):
         driverNumber = driverNumber - 1
 
     elif p[1]=='input':
-       p[0] = 'driver'+ str(driverNumber) +'.switchTo().activeElement().sendKeys('+param+')'
+       p[0] = 'driver'+ str(driverNumber) +'.switchTo().activeElement().sendKeys('+param[0]+')'
        
     elif p[1]=='inputE':
-       p[0] = 'driver'+ str(driverNumber) +'.findElement(By.name('+param[1]+').sendKeys('+param[0]+')'
+       p[0] = 'driver'+ str(driverNumber) +'.findElement(By.name('+param[1]+')).sendKeys('+param[0]+')'
        
     elif p[1]=='click':
         p[0] = 'driver'+str(driverNumber) + '.switchTo().activeElement().click()'
