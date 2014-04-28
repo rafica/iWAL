24c95edfc3785c41f7abd4a2cf28c719a12de0eb
nr2476 <nr2476@columbia.edu>
2014-04-27 23:41:40 -0400

    Final type_checker before writing functions

--- compiler/nithin.py
+++ compiler/nithin.py
@@ -6,7 +6,6 @@ def function_definition_1(s, temp, scope):
 def declaration_statement_1(s, temp, scope):
     s[(temp.children[1], scope)] = [temp.children[0].datatype]
     temp.code = temp.children[0].code + ' ' +str(temp.children[1]) + ';\n'
-    return scope
 
 ##def declaration_statement_2(s, temp, scope):
 ##    s[(temp.children[1], scope)] = 
--- compiler/typechecker.py
+++ compiler/typechecker.py
@@ -9,12 +9,18 @@ func_dict = {"declaration_statement_1" : nithin.declaration_statement_1,
              }
 scope = 0
 
+scope_incrementers = ['function_definition_1', 'function_definition_2', 'iteration_statement_1', 'iteration_statement_2', 'selection_statement_1', 'selection_statement_2']
+
 def postorder(root, scope):
     if type(root) != Node:
         return
     else:
-        if root.type=='function_definition_1' or root.type=='function_definition_2':
+        if root.type in scope_incrementers:
             scope = scope + 1
         for i in root.children:
             postorder(i, scope)
-        scope = func_dict[root.type](symbol_table, root, scope)
+            
+        func_dict[root.type](symbol_table, root, scope)
+        
+        if root.type in scope_incrementers:
+            scope = scope - 1
