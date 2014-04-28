f67d6bae561a8e27d9ba0903680563dcd17615a3
pranita <sushchat123@dyn-160-39-224-128.dyn.columbia.edu>
2014-04-28 02:24:31 -0400

    Added some pranita.py

---
+++ compiler/pranita.py
@@ -0,0 +1,141 @@
+def getTypeFromSymTable(t, symTab, curr_scope):
+    flag = False
+    while curr_scope > 0:
+        if curr_scope in symTab:
+            if t in symTab[curr_scope]:
+                flag = True
+                return symTab[curr_scope][t][0]
+        curr_scope = curr_scope - 1    
+    if(not flag):
+        return False
+
+
+
+def relational_expression_1(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+
+def relational_expression_2(s, temp, scope):
+        if temp.children[0].datatype != temp.children[1].datatype:
+            print 'Trying to compare ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+            temp.datatype = 'error'
+            temp.code = 'errorerrorerror'
+        else:
+            temp.datatype = 'boolean'
+            temp.code = temp.children[0].code + ' < ' + temp.children[1].code 
+
+
+
+def relational_expression_3(s, temp, scope):
+        if temp.children[0].datatype != temp.children[1].datatype:
+            print 'Trying to compare ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+            temp.datatype = 'error'
+            temp.code = 'errorerrorerror'
+        else:
+            temp.datatype = 'boolean'
+            temp.code = temp.children[0].code + ' > ' + temp.children[1].code 
+
+   
+
+def relational_expression_4(s, temp, scope):
+        if temp.children[0].datatype != temp.children[1].datatype:
+            print 'Trying to compare ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+            temp.code = 'errorerrorerror'
+            temp.datatype = 'error'
+        else:
+            temp.datatype = 'boolean'
+            temp.code = temp.children[0].code + ' <= ' + temp.children[1].code 
+
+   
+def relational_expression_5(s, temp, scope):
+        if temp.children[0].datatype != temp.children[1].datatype:
+            print 'Trying to compare ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+            temp.datatype = 'error'
+            temp.code = 'errorerrorerror'
+        else:
+            temp.datatype = 'boolean'
+            temp.code = temp.children[0].code + ' >= ' + temp.children[1].code 
+
+
+
+
+def logical_AND_expression_1(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+
+
+def logical_AND_expression_2(s, temp, scope):
+    if temp.children[0].datatype == 'boolean' and temp.children[1].datatype == 'boolean':
+        temp.datatype = temp.children[0].datatype
+        temp.code = temp.children[0].code + ' && ' + temp.children[1].code
+    else:
+        print 'Trying to AND ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+        temp.code = 'errorerrorerror'
+        temp.datatype = 'error'
+        
+def primary_expression_1(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = '( ' + temp.children[0].code + ' )'
+
+
+def primary_expression_2(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+
+
+def primary_expression_3(s, temp, scope):
+    flag = getTypeFromSymTable(temp.children[0], s, scope)
+    if flag == False:
+        print 'Trying to access ' + temp.children[0] + ', but not defined!'
+        temp.code = 'errorerrorerror'
+        temp.datatype = 'error'
+    else:
+        temp.datatype = flag
+        temp.code = temp.children[0]
+
+def primary_expression_4(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+
+
+def primary_expression_5(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+
+
+def multiplicative_expression_1(s, temp, scope):
+    temp.datatype = temp.children[0].datatype
+    temp.code = temp.children[0].code
+    
+
+def multiplicative_expression_2(s, temp, scope):
+    if temp.children[0].datatype != temp.children[1].datatype:
+        print 'Trying to multiply ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+        temp.datatype = 'error'
+        temp.code = 'errorerrorerror'
+    else:
+        temp.datatype = temp.children[0].datatype
+        temp.code = temp.children[0].code + ' * ' + temp.children[1].code 
+    
+def multiplicative_expression_3(s, temp, scope):
+    if temp.children[0].datatype != temp.children[1].datatype:
+        print 'Trying to divide ' + temp.children[0].datatype + ' and' + temp.children[1].datatype + '!'
+        temp.datatype = 'error'
+        temp.code = 'errorerrorerror'
+    else:
+        temp.datatype = temp.children[0].datatype
+        temp.code = temp.children[0].code + ' / ' + temp.children[1].code 
+    
+
+            
+
+    
+
+
+
+
+
+
+
+            
+            
