2a9cf46a10e2029a94649ec1df2a78c09ec62c43
pranita <sushchat123@dyn-160-39-224-219.dyn.columbia.edu>
2014-05-08 04:16:23 -0400

    commented subprocess in yaccer

--- compiler/iyaccer.py
+++ compiler/iyaccer.py
@@ -564,24 +564,24 @@ if __name__=="__main__":
             f.close()
 
             # ## Running the target program generated
-            javaFileName = 'Target'
-
-            # ## Running the target program generated    # javaFileName = 'Target'
-
-            p1 = subprocess.Popen('javac -classpath selenium-server-standalone-2.39.0.jar '+javaFileName+'.java', stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-            (output1, err1) = p1.communicate()
-
-            if err1 == '':
-
-                print 'Compiled!..\n'
-
-                p2 = subprocess.Popen('java -cp .:selenium-server-standalone-2.39.0.jar ' + javaFileName, stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
-                (output2, err2) = p2.communicate()
-
-                if err2 == '':
-                    print 'Output:\n', output2
-                else:
-                    print 'Error:\n', err2
-
-            else:
-                print 'Compile time error:\n' + err1
+##            javaFileName = 'Target'
+##
+##            # ## Running the target program generated    # javaFileName = 'Target'
+##
+##            p1 = subprocess.Popen('javac -classpath selenium-server-standalone-2.39.0.jar '+javaFileName+'.java', stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+##            (output1, err1) = p1.communicate()
+##
+##            if err1 == '':
+##
+##                print 'Compiled!..\n'
+##
+##                p2 = subprocess.Popen('java -cp .:selenium-server-standalone-2.39.0.jar ' + javaFileName, stdout=subprocess.PIPE, stderr = subprocess.PIPE, shell=True)
+##                (output2, err2) = p2.communicate()
+##
+##                if err2 == '':
+##                    print 'Output:\n', output2
+##                else:
+##                    print 'Error:\n', err2
+##
+##            else:
+##                print 'Compile time error:\n' + err1
