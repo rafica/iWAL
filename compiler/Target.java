0a6b1bd88586d00e8c851e389b06f2cf1073d9d2
nr2476 <nr2476@columbia.edu>
2014-05-08 22:08:31 -0400

    no changes

--- compiler/Target.java
+++ compiler/Target.java
@@ -9,7 +9,12 @@ public class Target  {
 
 public static void main(String[] args) {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
-String s = new String(System.console().readPassword());
+WebDriver drivera = new ChromeDriver();
+drivera.get( "https://www.google.com");
+for(int loop1=0;loop1< 2;loop1++)
+drivera.switchTo().activeElement().sendKeys(Keys.TAB);
+drivera.switchTo().activeElement().sendKeys( "Search Query");
+System.out.println("hello, world"+String.valueOf( 1.0));
 
 }
 }
