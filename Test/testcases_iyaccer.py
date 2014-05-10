123c7e7ace68c9910144da267135e6598a443575
aarora91 <ashima.arora91@gmail.com>
2014-05-09 23:24:39 -0400

    Done with yaccer test cases.

--- Test/testcases_iyaccer.py
+++ Test/testcases_iyaccer.py
@@ -24,11 +24,40 @@ class iWALTestCases(object):
     }"""
 
     function_definition = """
-    {
     void findSum(int a,int b){
-        int result = a+b;
+        int result = a+b;}
+    """
+
+    break_statement = """
+    {int i=1;
+    if(i>3){
+    break;
+    }
+    else{
+    i=i+1;
+    }
+    print(i);
+    }"""
+
+    continue_statement = """
+    {
+    repeat(3){
+        print("test");
     }
     }"""
-    
+
+    function_call = """
+    void printNum(double x){
+        print(x);
+    }
+    printNum(5);
+    """
+
+    declarations = """
+    int x=2;
+    double y=0.23;
+    boolean z=true;
+    key press=enter;
+    """
     def __init__(self):
         pass
--- Test/testing_iyaccer.py
+++ Test/testing_iyaccer.py
@@ -4,14 +4,13 @@ from testcases_iyaccer import iWALTestCases
 sys.path.append('../compiler')
 import iyaccer
 
-##parser = iyaccer.testingYacc()
-parser = iyaccer.mainYacc()
-print 'here..'
-##for attr in iWALTestCases.__dict__.keys():
-##    if not re.match(r'^__.*__$', attr):
-####        print getattr(iWALTestCases, attr)
-##        result = parser.parse(getattr(iWALTestCases, attr))
-##        print result
-##        f = open('testfiles/' + attr, 'w') 
-##        f.write(result.__str__())
-##        f.close()
+parser = iyaccer.testingYacc()
+
+for attr in iWALTestCases.__dict__.keys():
+    if not re.match(r'^__.*__$', attr):
+        print getattr(iWALTestCases, attr)
+        result = parser.parse(getattr(iWALTestCases, attr))
+        print result
+        f = open('testfiles/' + attr+'.txt', 'w') 
+        f.write(result.__str__())
+        f.close()
