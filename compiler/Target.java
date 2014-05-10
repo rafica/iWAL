a4317dffc41a8a62f7c579389bb52eaf47691474
nr2476 <nr2476@columbia.edu>
2014-05-10 00:37:32 -0400

    Testing functions

--- compiler/Target.java
+++ compiler/Target.java
@@ -15,20 +15,9 @@ static Actions builder;
 
 public static void main(String[] args) throws Exception {
         System.setProperty("webdriver.chrome.driver", "chromedriver");
-WebDriver drivera = new ChromeDriver();
-drivera.get( "https://www.surveymonkey.com/s/5WD55L2");
-for(int loopa1=0;loopa1< 1;loopa1++)
-drivera.switchTo().activeElement().sendKeys(Keys.TAB);
-String s = new Scanner(System.in).next();
-drivera.switchTo().activeElement().sendKeys( s);
-for(int loopa1=0;loopa1< 1;loopa1++)
-drivera.switchTo().activeElement().sendKeys(Keys.TAB);
-s=new Scanner(System.in).next();
-drivera.switchTo().activeElement().sendKeys( s);
-for (int loop2 = 0; loop2 <7 ; loop2++){
-for(int loopa2=0;loopa2< 1;loopa2++)
-drivera.switchTo().activeElement().sendKeys(Keys.TAB);
- drivera.switchTo().activeElement().sendKeys( "This is the text to be filled ...");
+int x = 5;
+for (int loop2 = 0; loop2 <x ; loop2++){
+System.out.println("Hey ...");
 
 }
 
